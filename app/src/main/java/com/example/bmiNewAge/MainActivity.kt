339794dab5a0d42955ce9938d8458b0925508d36
RyanKoech <sirryankoech@gmail.com>
2022-10-15 11:44:45 +0300

    Implimented splashscreen

--- app/src/main/java/com/example/bmiNewAge/MainActivity.kt
+++ app/src/main/java/com/example/bmiNewAge/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.bmiNewAge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
