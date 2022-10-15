6ebeced5eca30896da9b3bf9e2752dbbb00ae163
RyanKoech <sirryankoech@gmail.com>
2022-10-15 12:15:58 +0300

    Implemented frag host and navigation

--- app/src/main/java/com/example/bmiNewAge/MainActivity.kt
+++ app/src/main/java/com/example/bmiNewAge/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.bmiNewAge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
