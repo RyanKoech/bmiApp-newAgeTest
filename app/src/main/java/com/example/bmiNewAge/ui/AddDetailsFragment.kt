bf89792879e922b46599b5b9207936525b5916be
RyanKoech <sirryankoech@gmail.com>
2022-10-18 10:11:58 +0300

    Implemented addfragments fragment logic

--- app/src/main/java/com/example/bmiNewAge/ui/AddDetailsFragment.kt
+++ app/src/main/java/com/example/bmiNewAge/ui/AddDetailsFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.ValueCallback
-import com.example.bmiNewAge.R
+import androidx.navigation.fragment.findNavController
 import com.example.bmiNewAge.databinding.FragmentAddDetailsBinding
+import com.example.bmiNewAge.other.BmiCategories
+import com.example.bmiNewAge.other.Constants
+import com.example.bmiNewAge.other.Utilities
 
 class AddDetailsFragment : Fragment() {
 
@@ -21,8 +23,52 @@ class AddDetailsFragment : Fragment() {
 
         _binding = FragmentAddDetailsBinding.inflate(inflater, container, false)
 
+        initializeViewElements()
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun initializeViewElements() {
+        binding.numberPickerWeight.apply {
+            minValue = Constants.MIN_WEIGHT
+            maxValue = Constants.MAX_WEIGHT
+            value = Constants.BASE_WEIGHT
+        }
+
+        binding.numberPickerHeight.apply {
+            minValue = Constants.MIN_HEIGHT
+            maxValue = Constants.MAX_HEIGHT
+            value = Constants.BASE_HEIGHT
+        }
+
+        binding.numberPickerGender.apply {
+            minValue = 0
+            maxValue = Utilities.genders.size-1
+            value = 0
+            displayedValues = Utilities.getGenderString()
+        }
+
+        binding.buttonCalculate.setOnClickListener {
+            buttonCalculateOnClick()
+            // this@AddDetailsFragment.findNavController().navigate(R.id.action_addDetailsFragment_to_resultsFragment)
+        }
+    }
+
+    private fun buttonCalculateOnClick() {
+        val name = binding.editTextName.text.toString()
+
+        if(name.isBlank()){
+            binding.editTextName.error = "Please enter your name."
+            return
+        }
+
+        val weight : Int = binding.numberPickerWeight.value
+        val height : Int = binding.numberPickerHeight.value
+
+        val action  = AddDetailsFragmentDirections.actionAddDetailsFragmentToResultsFragment(weight.toFloat(), height.toFloat(), name)
+        findNavController().navigate(action)
+
     }
 
 }
