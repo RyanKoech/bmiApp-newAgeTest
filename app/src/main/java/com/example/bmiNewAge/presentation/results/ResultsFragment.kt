0cf298073af77d6c3e620a382285fe4df5d33ab8
RyanKoech <sirryankoech@gmail.com>
2022-10-22 18:47:57 +0300

    Implemented functionality to take and save a screenshot of the bmi results

--- app/src/main/java/com/example/bmiNewAge/presentation/results/ResultsFragment.kt
+++ app/src/main/java/com/example/bmiNewAge/presentation/results/ResultsFragment.kt
@@ -1,11 +1,18 @@
 package com.example.bmiNewAge.presentation.results
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.bmiNewAge.R
@@ -14,6 +21,11 @@ import com.example.bmiNewAge.common.Utilities
 import com.google.android.ads.nativetemplates.NativeTemplateStyle
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
 
 class ResultsFragment : Fragment() {
 
@@ -65,6 +77,69 @@ class ResultsFragment : Fragment() {
             textViewPonderalResult.text = getString(R.string.message_ponderal_index, ponderalIndex)
             textViewBmiWhole.text = formattedBmi[0]
             textViewBmiDecimal.text = formattedBmi[1]
+            buttonShare.setOnClickListener{
+                saveImage()
+            }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // saveImage()
+        }else {
+            Toast.makeText(this.requireActivity(), "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun saveImage() {
+        if(!checkPermission()) return
+
+        try {
+            val path : String = Environment.getExternalStorageDirectory().toString() + "/DCIM/" + getString(R.string.app_name)
+            val fileDir = File(path)
+            if(!fileDir.exists())
+                fileDir.mkdir()
+
+            val mPath : String = path + "/bmi_screenshot_" + Calendar.getInstance().timeInMillis.toString() + ".png"
+            
+            val bitmap : Bitmap = getScreenshot()
+            val file = File(mPath)
+            val fOut = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut)
+            fOut.flush()
+            fOut.close()
+
+            Toast.makeText(this.requireActivity(), "Image Saved Successfully", Toast.LENGTH_LONG).show()
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getScreenshot(): Bitmap {
+        val view : View = this.requireView().findViewById(R.id.linearLayout_results)
+        val bitmap : Bitmap = Bitmap.createBitmap(
+            view.width,
+            view.height,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    private fun checkPermission() : Boolean {
+        val permission : Int = ActivityCompat.checkSelfPermission(this.requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if(permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            return false
+        }
+        return true
     }
 }
